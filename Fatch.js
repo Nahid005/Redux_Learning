1ad3304590cc7806ed07b8c687ad06b764d89fab
Nahid005 <nahidhassan9258@gmail.com>
2022-11-09 00:06:21 +0600

    data fatch

--- Fatch.js
+++ Fatch.js
@@ -1,7 +1,75 @@
 const {createStore, applyMiddleware} = require("redux");
+const { default: logger } = require("redux-logger");
+
 /**
  * Action 
  * Reducer
  * Store
  */
+
+// constant value 
+
+const GET_USER_URL = "GET_USER_URL"
+const GET_SUCCESS_USER = "GET_SUCCESS_USER"
+const GET_FAILED_USER = "GET_FAILED_USER"
+
+const initialState = {
+    user: [],
+    isLoading: false,
+    error: null
+}
+
+// action 
+
+const getAllUser = () => {
+    return {
+        type:GET_USER_URL
+    }
+}
+
+const getSuccessUser = (users) => {
+    return {
+        type:GET_SUCCESS_USER,
+        payload: users,
+    }
+}
+
+const getFailedUser = (error) => {
+    return {
+        type:GET_FAILED_USER,
+        payload:error
+    }
+}
+
+// reducer handle 
+
+const getDataReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case GET_USER_URL:
+            return{
+                isLoading: true,
+                ...state,
+            }
+
+        case GET_SUCCESS_USER:
+            return{
+                isLoading: false,
+                ...state,
+                user:[...state.user, action.payload]
+            }
+        case GET_FAILED_USER:
+            return{
+                ...state,
+                error: action.payload
+            }
+        default:
+            state
+
+    }
+}
+
+const store = createStore(getDataReducer, applyMiddleware(logger))
+store.subscribe(()=> {
+    console.log(getState())
+})
 
